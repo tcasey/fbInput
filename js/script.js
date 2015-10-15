514b7b7736e39ff37224cd4e9ace684e0e0055b6
tcasey <trevort.casey@gmail.com>
2015-10-15 15:56:24 -0600

    Input added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	
-	for (var num = 1; num <= 100; num++){
-	
-	if (num % 5=== 0 && num % 3=== 0){
-		//console.log("Fizz Buzz")
-		$('.main').append("<p>Fizz Buzz</p>");
-	}
-	else if (num % 5 === 0){
-		//console.log ("buzz");
-		$('.main').append("<p>Buzz</p>");
+	$("button").on("click", function() {
 
-	}
-	else if (num % 3 === 0){
-		//console.log ("fizz");
-		$('.main').append("<p>Fizz</p>")
-	}
-	else {
-		//console.log(i);
-		$('.main').append("<p>" + num +"</p>");
-	}
+		$(".output").empty();
+		var someNumber = document.getElementById('someNumber').value; 
 
-}
+		var fizzbuzz = function (arg) {
+		for (var i = 1; i <= arg; i++) {
+			if (i % 5 === 0 && i % 3 === 0) { //check to see if i is divisible by 5 AND 3
+				$(".area").append("FizzBuzz <br>");
+				console.log("FizzBuzz");
+			} else if (i % 5 === 0) { //check to see if i is divisible by 5
+				$(".area").append("buzz <br>");
+				console.log("buzz");
+			} else if (i % 3 === 0) { //check to see if i is divisible by 3
+				$(".area").append("fizz <br>");
+				console.log("fizz");
+			} else {
+				$(".area").append(i + "<br>");
+				console.log(i); //if i doesn't fall under the 3 cases below, print 
+			}
+		}
+		}
+
+	fizzbuzz(someNumber);
+
 	});
+
+	$("#someNumber").keypress(function(e){  //listens for enter keydown and calls the button click event
+		if(e.which == 13) {
+			$('button').click();
+		}
+	});
+});
